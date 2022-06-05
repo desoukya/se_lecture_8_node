636a15b2a1c7a1eddb3c882b9bdb40aa31baaedd
Amr Desouky <desoukya@gmail.com>
2022-06-05 17:06:50 -0400

    add create orders endpoint and return all products endpoint

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 require('dotenv').config();
+const bodyParser = require('body-parser')
 const express = require('express');
 const axios = require('axios');
+const { uuid } = require('uuidv4');
 const { mongoClient } = require('./mongo');
+// const products = require('./products.json');
+const products = require('./test_products.json');
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.all('*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 
 app.get('/', async (req,res) => {
   const db = await mongoClient();
@@ -13,6 +25,30 @@ app.get('/', async (req,res) => {
   await db.collection('weather').insertOne(data);
 
   return res.send(data);
+});
+
+app.get('/products', async (req,res) => {
+  return res.json(products);
+});
+
+app.post('/orders', async (req,res) => {
+  const db = await mongoClient();
+  if (!db) res.status(500).send('Systems Unavailable');
+
+  const newOrder = {
+    name: req.body.name,
+    price: req.body.price,
+    quantity: 1,
+    id: uuid(),
+  };
+  await db.collection('orders').insertOne(newOrder);
+
+  return res.send(newOrder);
+});
+
+app.get('/products/:id', async (req,res) => {
+  const product = products.filter(({ id }) => req.params.id === id);
+  return res.json(product.length ? product.shift() : product);
 });
 
 app.listen(3000);
